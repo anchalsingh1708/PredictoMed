418be5f0e24920163b630bb375fd1c7ae3c8efb0
anchalsingh <94751114+anchalsingh1708@users.noreply.github.com>
2023-02-14 19:26:25 +0530

    Update multiple_disease_prediction.py

--- multiple_disease_prediction.py
+++ multiple_disease_prediction.py
@@ -261,10 +261,9 @@ if st.button('Heart Disease Test Result'):
 		heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg,thalach,exang,oldpeak,slope,ca,thal]])
 		if (heart_prediction[0] == 1):
 			heart_diagnosis = 'The person is having heart disease'
-                else:
+		else:
 			heart_diagnosis = 'The person does not have any heart disease'
-                
-        st.success(heart_diagnosis)
+	st.success(heart_diagnosis)
         
             
         
